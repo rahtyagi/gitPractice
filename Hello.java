40e378174647f230c16e29b52c109cf72ab3b62d
Cloud User <ec2-user@ip-172-31-19-6.us-west-1.compute.internal>
2023-10-04 16:32:58 +0000

    Second commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 public static void main(String as[])
 {
-System.out.println("Hello how are you");
+System.out.println("Hello how are you are");
 }
 }
---
+++ Hello2.java
@@ -0,0 +1,6 @@
+class Hello2
+{
+public static void  main(String  sd)
+{
+Syso("b");}
+}
