2c79c9302d34a688ec39998177809c54baeef053
Rahul Tyagi <r.tyagi1@gmail.com>
2024-05-15 23:36:02 +0530

    Remove all

--- Hello.java
+++
@@ -1,8 +0,0 @@
-class Hello
-{
-public static void main(String as[])
-{
-System.out.println("Hello master ji");
-System.out.println("Hello master");
-}
-}
--- Hello2.java
+++
@@ -1,7 +0,0 @@
-class Hello2
-{
-public static void  main(String  sd)
-{
-Syso("feature 102 branch");
-}
-}
